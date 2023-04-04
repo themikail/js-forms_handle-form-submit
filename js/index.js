3699fdf581dc35492eb4bda019f9be432305b4bd
themikail <hello@mikailgokce.com>
2023-04-04 14:52:34 +0200

    add js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(form);
+
+  const data = {};
+  formData.forEach((value, key) => (data[key] = value));
+
+  console.log(data);
+});
